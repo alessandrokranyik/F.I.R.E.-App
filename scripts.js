992f04154a5eb678016af9b492b84359692f97d6
alessandrokranyik <alessandroj.kranyik@gmail.com>
2021-03-03 10:19:31 +0100

    Test

--- scripts.js
+++ scripts.js
@@ -4,27 +4,29 @@ function buttonFunction(){
 }
 
 function zuwachsBerechnen(){
+    //Benutzer Eingabe {Startkapital, Zinssatz}
     var anfangskapital = document.querySelector('#anlagen').value;
     var zinssatz = document.querySelector('#compoundeffect').value;
     anfangskapital = parseInt(anfangskapital,10);
     zinssatz = parseInt(zinssatz,10);
     
-    
+    //Benutzer Eingabe {Ausgaben}, Berechnung benötigten Kapitals & Dauer
     ausgaben = document.getElementById('ausgaben').value;
     noetigeskapital = (ausgaben * 30);
     dauer = (noetigeskapital / anfangskapital);
-
     var kontostand = anfangskapital;
     var laufzeit = dauer;
 
-    for(jahr = 0; jahr < laufzeit; jahr++){
+    for(jahr = 1; jahr < laufzeit; jahr++){
         var zinsen = kontostand * zinssatz /100;
         kontostand = kontostand + zinsen;
         kontostand += anfangskapital;
 
-        console.log(jahr);
-        console.log('Zinsen:' + zinsen.toFixed(0));
-        console.log('Konto:' + kontostand.toFixed(0));
+        console.log('Jahr ' + jahr + '.');
+        console.log('Zinsen:' + zinsen.toFixed(0) + '€');
+        console.log('+ ' + anfangskapital + '€');
+        console.log('Konto:' + kontostand.toFixed(0) + '€');
+        console.log('');
         if(kontostand > noetigeskapital) {
             console.log(jahr);
             dauer = jahr;
@@ -33,8 +35,8 @@ function zuwachsBerechnen(){
     }
 
     dauerErgebnis = document.getElementById('p02');
-    var nachricht2 = "In " + dauer + " Jahren sind sie am Ziel. Ihr Kontostand beträgt dann: " + kontostand.toFixed(2) + "€";
-    dauerErgebnis.innerHTML = nachricht2;
+    var ergebnis = "In " + dauer + " Jahren sind sie am Ziel. Ihr Kontostand beträgt dann: " + kontostand.toFixed(2) + "€";
+    dauerErgebnis.innerHTML = ergebnis;
 }
 
 function kapitalBerechnen() {
